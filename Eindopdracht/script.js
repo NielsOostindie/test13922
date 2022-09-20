f253e974215b801c9e4f627349d8d131fc434ca3
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2022-09-20 09:14:31 +0200

    commit11

--- Eindopdracht/script.js
+++ Eindopdracht/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-    var docEl = $(document),
-        headerEl = $('ul'),
-        headerWrapEl = $('.wrapHead'),
-        navEl = $('nav'),
-        linkScroll = $('.scroll');
+    var docEl = $(document),// selector die die document beet pakt
+        headerEl = $('ul'),// selector die die ul beet pakt
+        headerWrapEl = $('.wrapHead'),//selector die die wraphead beet pakt
+        navEl = $('nav'),// selector die die nav beet pakt
+        linkScroll = $('.scroll');// selector die die scroll beet pakt
 
-    docEl.on('scroll', function () {
-        if (docEl.scrollTop() > 260) {
-            headerEl.addClass('fixed-to-top');
-            headerWrapEl.addClass('fixed-to-top');
-            navEl.addClass('fixed-to-top');
+    docEl.on('scroll', function () { // vuurt function uit op scroll
+        if (docEl.scrollTop() > 260) {// als de bovenkant op 260 hoog is
+            headerEl.addClass('fixed-to-top');// houd de ul altijd boven
+            headerWrapEl.addClass('fixed-to-top');// houd de wraphead altijd boven
+            navEl.addClass('fixed-to-top');// houd de nav altijd boven
         }
         else {
             headerEl.removeClass('fixed-to-top');
